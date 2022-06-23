91ea1630be5e4843c931d61bffaf9456fd7c05d4
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2022-06-23 11:14:39 -0500

    more work done on this

--- lib/VM/Libvirt/CloneHelper.pm
+++ lib/VM/Libvirt/CloneHelper.pm
@@ -3,6 +3,7 @@ package VM::Libvirt::CloneHelper;
 use 5.006;
 use strict;
 use warnings;
+use File::Slurp;
 
 =head1 NAME
 
@@ -10,43 +11,153 @@ VM::Libvirt::CloneHelper - The great new VM::Libvirt::CloneHelper!
 
 =head1 VERSION
 
-Version 0.01
+Version 0.0.1
 
 =cut
 
-our $VERSION = '0.01';
-
+our $VERSION = '0.0.1';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+    # initialize it
+    my $clone_helper=VM::Libvirt::CloneHelper->new({
+        blank_domains=>'/usr/local/etc/libvirt_clonehelper/blank_domains',
+        windows_blank=>0,
+        mac_base=>'00:08:74:2d:dd:',
+        ipv4_base=>'192.168.1.',
+        start=>'100',
+        to_clone=>'baseVM',
+        delete_old=>1,
+        clone_name_base=>'foo',
+        uuid_auto=>1,
+        count=>10,
+        verbose=>1,
+    });
 
-Perhaps a little code snippet.
+=head1 METHODS
 
-    use VM::Libvirt::CloneHelper;
+=head2 new
 
-    my $foo = VM::Libvirt::CloneHelper->new();
-    ...
+Initialize the module.
 
-=head1 EXPORT
+    network=>'default'
+    Name of the libvirt network in question.
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+    blank_domains=>'/usr/local/etc/libvirt_clonehelper/blank_domains',
+    List of domains to blank via setting 'dnsmasq:option value='address=/foo.bar/'.
+    If not this file does not exist, it will be skipped.
 
-=head1 SUBROUTINES/METHODS
+    windows_blank=>0,
+    Blank commonly used MS domains.
 
-=head2 function1
+    mac_base=>'00:08:74:2d:dd:',
+    Base to use for the MAC.
+
+    ipv4_base=>'192.168.1.',
+    Base to use for the IPs for adding static assignments.
+
+    start=>'100',
+    Where to start in 
+
+    to_clone=>'baseVM',
+    The name of the VM to clone.
+
+    delete_old=>1,
+    If VMs should be deleted if they already exist.
+    If not they will be skipped.
+
+    clone_name_base=>'cloneVM',
+    Base name to use for creating the clones. 'foo' will become 'foo-$current', so
+    for a start of 100, the first one would be 'foo-100' and with a count of 10 the
+    last will be 'foo-109'.
+
+    uuid_auto=>1,
+    Grab the UUID from the current config.
+
+    count=>10,
+    How many clones to create.
 
 =cut
 
-sub function1 {
+sub new {
+	my %args;
+	if ( defined( $_[1] ) ) {
+		%args = %{ $_[1] };
+	}
+
+	my $self = {
+		blank_domains   => '/usr/local/etc/libvirt_clonehelper/blank_domains',
+		windows_blank   => 0,
+		mac_base        => '00:08:74:2d:dd:',
+		ipv4_base       => '192.168.1.',
+		start           => '100',
+		to_clone        => 'baseVM',
+		delete_old      => 1,
+		clone_name_base => 'foo',
+		uuid_auto       => 1,
+		count           => 10,
+	};
+	bless $self;
+
+	# do very basic value sanity checks and reel values in
+	my @keys = keys(%args);
+	foreach my $key (@keys) {
+		if ( $key eq 'mac_base' ) {
+
+			# make sure we got a sane base MAC
+			if ( $args{mac_base}
+				!~ /^[0-9aAbBcCdDeEfF][0-9aAbBcCdDeEfF]\:[0-9aAbBcCdDeEfF][0-9aAbBcCdDeEfF]\:[0-9aAbBcCdDeEfF][0-9aAbBcCdDeEfF]\:[0-9aAbBcCdDeEfF][0-9aAbBcCdDeEfF]\:[0-9aAbBcCdDeEfF][0-9aAbBcCdDeEfF]\:$/
+				)
+			{
+				die( '"' . $args{mac_base} . '" does not appear to be a valid base for a MAC address' );
+			}
+		}
+		elsif ( $key eq 'ipv4_base' ) {
+			# make sure we have a likely sane base for the IPv4 address
+			if ( $args{ipv4_base} !~ /^[0-9]+\.[0-9]+\.[0-9]+\.$/ ) {
+				die( '"' . $args{ipv4_base} . '" does not appear to be a valid base for a IPv4 address' );
+			}
+		}
+		elsif ( $key eq 'to_clone' ) {
+			# make sure we have a likely sane base VM name
+			if ( $args{to_clone} !~ /^[A-Za-z0-9\-\.]+$/ ) {
+				die( '"' . $args{to_clone} . '" does not appear to be a valid VM name' );
+			}
+		}
+		elsif ( $key eq 'clone_name_base' ) {
+			# make sure we have a likely sane base name to use for creating clones
+			if ( $args{clone_name_base} !~ /^[A-Za-z0-9\-\.]+$/ ) {
+				die( '"' . $args{clone_name_base} . '" does not appear to be a valid VM name' );
+			}
+		}
+
+		# likely good, adding
+		$self->{$key} = $args{$key};
+	}
+
+	return $self;
 }
 
-=head2 function2
+=head2 delete_clones
+
+Delete all the clones
+
+    $clone_helper->delete_clones;
 
 =cut
 
-sub function2 {
+sub delete_clones{
+	my $self=$_[0];
+
+	# virsh undefine --snapshots-metadata
+	# the VM under /var/lib/libvirt/images needs to be removed manually given
+	# the shit show that is libvirt does not have a means of sanely removing
+	# VMs and relevant storage... for example it will include ISOs in relevant
+	# VMs to be removed if you let it... and it is likely to fail to remove the
+	# base disk image for a VM, even if you pass it any/every combination of
+	# possible flags...
+
+	
 }
 
 =head1 AUTHOR
@@ -102,4 +213,4 @@ This is free software, licensed under:
 
 =cut
 
-1; # End of VM::Libvirt::CloneHelper
+1;    # End of VM::Libvirt::CloneHelper
